1f83a82180c21e22701e5f6bd8281f33fb03857f
Marjorie <Marjorie@192.168.1.14>
2021-09-22 20:49:02 -0600

    Clase fproducto completada

--- src/logica/fproducto.java
+++ src/logica/fproducto.java
@@ -1,5 +1,6 @@
 package logica;
-import Datos.vhabitacion;
+
+import Datos.vproductos;
 import java.sql.ResultSet;
 import java.sql.Connection;
 import javax.swing.table.DefaultTableModel;
@@ -8,18 +9,18 @@ import javax.swing.JOptionPane;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
 public class fproducto {
+
     private conexion mysql = new conexion(); //instancia para la conexion
     private Connection cn = mysql.conectar();// llama a la funcion conectar de la variable que instanciamos
     private String sSQL = "";//almacena la cadena de conexion
     public Integer totalregistros; // Total de registros tiene
 
-    public DefaultTableModel mostrar(String buscar) {  // Mostrar los registros de la tabla habitacion
+    public DefaultTableModel mostrar(String buscar) {  // Mostrar los registros de la tabla producto
         DefaultTableModel modelo;
 
         //Guarda los títulos de la columna
-        String[] titulos = {"ID","Producto", "Descripcion", "Precio"};
+        String[] titulos = {"ID", "Producto", "Descripcion", "Precio"};
         String[] registro = new String[4];  // almacena el registro de cada columna 
 
         totalregistros = 0;
@@ -34,9 +35,8 @@ public class fproducto {
             while (rs.next()) { //todos los registros obtenidos
                 registro[0] = rs.getString("idh_producto");
                 registro[1] = rs.getString("nombre");
-                registro[2] = rs.getString("descripcion");           
+                registro[2] = rs.getString("descripcion");
                 registro[3] = rs.getString("precio");
-             
 
                 totalregistros = totalregistros + 1; //Aumenta la variable en 1
                 modelo.addRow(registro);  //agrega a la variable modelo todos los registros
@@ -50,9 +50,72 @@ public class fproducto {
 
     }
 
-    
-    
-    
-    
-    
+    public boolean insertar(vproductos dts) {   // Funcion insertar, recibe todo lo de la clase vproductos
+        sSQL = "insert into h_producto(nombre,descripcion,precio)"
+                + "values (?,?,?)";
+        try {
+            PreparedStatement pst = cn.prepareStatement(sSQL);// prepara la cadena para poder insertar los registros
+            pst.setString(1, dts.getNombre()); //Enviar 1 a 1 todos los valores
+            pst.setString(2, dts.getDescripcion());
+            pst.setDouble(3, dts.getPrecio());
+
+            int n = pst.executeUpdate();//almacena el estado de la ejecucucion del Statement
+
+            if (n != 0) { //condicion para ver si se ingresaron registros
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {   // error si 
+            JOptionPane.showConfirmDialog(null, e);//Lanza el mensaje de error
+            return false;
+        }
+    }
+
+    public boolean editar(vproductos dts) {
+        sSQL = "update h_producto set nombre=?,descripcion=?,precio=?" //actualizar tabla producto
+                + "where idh_producto=?";
+
+        try {
+            PreparedStatement pst = cn.prepareStatement(sSQL);// prepara la cadena para poder insertar los registros
+            pst.setString(1, dts.getNombre()); //Enviar 1 a 1 todos los valores
+            pst.setString(2, dts.getDescripcion());
+            pst.setDouble(3, dts.getPrecio());
+            pst.setDouble(4, dts.getIdh_producto());
+
+            int n = pst.executeUpdate();//almacena el estado de la ejecucucion del Statement
+
+            if (n != 0) { //condicion para ver si se ingresaron registros
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) { //si tiene error
+            JOptionPane.showConfirmDialog(null, e); //lanza el mesaje de error
+            return false;
+        }
+    }
+
+    public boolean eliminar(vproductos dts) {
+        sSQL = "delete from h_producto where idh_producto=?";  // Borra los registros de los productos en el ID indicado
+
+        try {
+            PreparedStatement pst = cn.prepareStatement(sSQL); // prepara la cadena para poder insertar los registros
+            pst.setInt(1, dts.getIdh_producto());// El indice 1, es el ID producto para indicar que se elimina
+            int n = pst.executeUpdate(); //almacena el estado de la ejecucucion del Statement
+
+            if (n != 0) {   //Revisa si esta vacio
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null, e); //lanza el mesaje de error
+            return false;
+        }
+    }
+
 }
